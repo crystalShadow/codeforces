4e7fd8f59ee5e0b0d9d8a68113b4baf4585d8f36
crystalShadow <shashankshekhar1299@gmail.com>
2024-03-31 13:53:41 +0530

    problem submission success

---
+++ april-2024/round-937-div-4/stairPeakNeither.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+string func(int a, int b, int c){
+    if(a < b){
+        if(b < c) return "STAIR\n";
+        else if(b > c) return "PEAK\n";
+        else return "NONE\n";
+    }
+    else return "NONE\n";
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int a, b, c;
+        cin >> a >> b >> c;
+        cout << func(a, b, c);
+    }
+    return 0;
+}
